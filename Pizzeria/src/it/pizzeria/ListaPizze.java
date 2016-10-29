b7eae7d1d2a6aecfcdcb42a25501db86cd4eb3eb
kawkabwassim <kwkwassim@gmail.com>
2016-10-29 12:22:34 +0200

    il prof ha recuperato qualcosa

---
+++ Pizzeria/src/it/pizzeria/ListaPizze.java
@@ -0,0 +1,12 @@
+package it.pizzeria;
+
+import java.util.ArrayList;
+
+public class ListaPizze {
+	public static String pizze[] = {"margherita","asdsaas","adasdasdas"};
+	
+	// elenco pizze da fare
+	ArrayList<String> pizzedaFare = new ArrayList<String>();
+	// elenco pizze pronte
+	ArrayList<String> pizzePronte = new ArrayList<String>();
+}
---
+++ Pizzeria/src/it/pizzeria/Pizzaiolo.java
@@ -0,0 +1,21 @@
+package it.pizzeria;
+
+public class Pizzaiolo implements Runnable{
+
+	ListaPizze lp;
+	
+	public Pizzaiolo(ListaPizze lp) {
+		this.lp = lp;
+	}
+	
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		// verifica se c'è una pzza da fare
+		
+		// fa la pizza ( aspetta tot secondi)
+		
+		// avvisa la lsita pizze che la pizza è pronta
+	}
+
+}
